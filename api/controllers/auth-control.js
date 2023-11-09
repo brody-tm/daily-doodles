53130021022d6c90a2b7511e135b2a8f3d9f9728
Riley McAllister <rjm0048@mix.wvu.edu>
2023-11-09 11:24:04 -0500

    Update fields in db queries

--- api/controllers/auth-control.js
+++ api/controllers/auth-control.js
@@ -1,24 +1,21 @@
-import dbConnection from "../db-connect.js"
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import dbConnection from "../db-connect.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-// TODO modify as necessary to match our tablke format
-export function registerAccount(req,res) {
-    const getUsersQuery = "SELECT * FROM users WHERE username = ?";
+// TODO modify as necessary to match our table format
+export function registerAccount(req, res) {
+  const getUsersQuery = "SELECT * FROM users WHERE username = ?";
 
-    // perform query to see if user exists in database
-    dbConnection.query(getUsersQuery, TODO, dbRegisterCallback);
-}
-
-function dbRegisterCallback(error, data) {
+  // perform query to see if user exists in database
+  dbConnection.query(getUsersQuery, TODO, (error, data) => {
     // server error
-    if (error){
-        return res.status(500).json(error);
+    if (error) {
+      return res.status(500).json(error);
     }
 
     // user already exists in database
     if (data.length) {
-        return res.status(409).json("User already exists!");
+      return res.status(409).json("User already exists!");
     }
 
     // if we get here, create new user
@@ -28,52 +25,50 @@ function dbRegisterCallback(error, data) {
     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
     const addUserQuery =
-    "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+      "INSERT INTO users (`id`,`email`,`password`,`created_at`) VALUE (?)";
 
     const userData = [
-    req.body.username,
-    req.body.email,
-    hashedPassword,
-    req.body.name,
+      req.body.id,
+      req.body.email,
+      hashedPassword,
+      req.body.createdAt,
     ];
 
     // perform query to add user to database
-    dbConnection.query(q, [userData], dbAddUserCallback);
-}
-
-function dbAddUserCallback(error, data) {
-    // server error
-    if (error) {
+    dbConnection.query(addUserQuery, [userData], (error, data) => {
+      // server error
+      if (error) {
         return res.status(500).json(error);
-    }
+      }
 
-    // successfully created user
-    return res.status(200).json("User has been created.");
+      // successfully created user
+      return res.status(200).json("User has been created.");
+    });
+  });
 }
-
 
 export function login(req, res) {
-    const q = "SELECT * FROM users WHERE username = ?";
+  const q = "SELECT * FROM users WHERE username = ?";
 
-    dbConnection.query(q, [req.body.username], dbLoginCallback);
-  };
-  
-function dbLoginCallback(error, data) {
+  dbConnection.query(q, [req.body.username], (error, data) => {
     // server error
     if (error) {
-        return res.status(500).json(error);
+      return res.status(500).json(error);
     }
     // user does not exist in database
     if (data.length === 0) {
-        return res.status(404).json("User not found!");
+      return res.status(404).json("User not found!");
     }
 
     // TODO figure out what this does
-    const checkPassword = bcrypt.compareSync(req.body.password, data[0].password);
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
 
     // invalid login credentials
     if (!checkPassword) {
-        return res.status(400).json("Wrong password or username!");
+      return res.status(400).json("Wrong password or username!");
     }
 
     // create token for login session
@@ -84,17 +79,21 @@ function dbLoginCallback(error, data) {
 
     // TODO figure out what this does
     res
-    .cookie("accessToken", token, {
+      .cookie("accessToken", token, {
         httpOnly: true, // TODO check if we want this
-    })
-    .status(200)
-    .json(others);
-  }
+      })
+      .status(200)
+      .json(others);
+  });
+}
 
 export function logout() {
-    // TODO figure out what this does
-    res.clearCookie("accessToken",{
-        secure:true,
-        sameSite:"none"
-    }).status(200).json("User has been logged out.")
+  // TODO figure out what this does
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out.");
 }
