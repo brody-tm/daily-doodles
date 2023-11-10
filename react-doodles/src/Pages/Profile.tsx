860b82b00a02107d0f0c8f2e2861d0d642c7cb8d
sourpatchkov <alanosnovnoy@gmail.com>
2023-11-09 20:46:13 -0500

    profile added with dynamic capability

--- react-doodles/src/Pages/Profile.tsx
+++ react-doodles/src/Pages/Profile.tsx
@@ -1,6 +1,21 @@
 import Heading from "../Components/Heading";
+import LeftSide from "../Components/ProfileLeft"
 import "../Styles/profile.css";
+import MyGallary from "../Components/profileGallary"
+import Awards from "../Components/profileAwards"
 function Profile() {
-  return <Heading text="Profile"></Heading>;
+  return( 
+    <div>
+    <Heading text="Profile"/>
+    <div className ="outside">
+    <div className='containerprofile'>
+   <LeftSide></LeftSide>
+    <MyGallary></MyGallary>
+    <Awards></Awards>
+    </div>
+  </div>
+  </div>
+  
+  );
 }
 export default Profile;
