9f4f48c991eea3e32d51ee3d12c5254dc806e2c1
pcsorrjr <pcs00008@mix.wvu.edu>
2023-11-10 12:08:42 -0500

    caption changes

--- react-doodles/src/Components/Caption.tsx
+++ react-doodles/src/Components/Caption.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import '../Styles/Caption.css'; // Import your CSS file
 
 interface Props {
   text: string;
@@ -7,19 +7,15 @@ interface Props {
 
 const Caption = ({ text }: Props) => {
   return (
-    <>
-      <div className="form-outline">
-        <textarea
-          className="form-control"
-          id="textAreaExample"
-          rows={4}
-          cols={20}
-        ></textarea>
-        <label className="form-label" htmlFor="textAreaExample">
-          {text}
-        </label>
-      </div>
-    </>
+    <div className="caption-container">
+      <textarea
+        className="caption-textbox"
+        placeholder="Caption your masterpiece here!"
+        rows={4}
+        cols={20}
+      ></textarea>
+      <label className="caption-label">{text}</label>
+    </div>
   );
 };
 
