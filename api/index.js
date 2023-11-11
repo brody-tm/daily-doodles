c4398d9bfd208ba00539875e3e6df6776e31e268
Izaak Whetsell <izaakwhetsell@gmail.com>
2023-11-11 13:27:49 -0500

    changed accounts controller and route to profile

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 import postRoutes from "./routes/posts-route.js";
 import likeRoutes from "./routes/likes-route.js";
 import commentRoutes from "./routes/comments-route.js";
-import accountRoutes from "./routes/accounts-route.js";
+import profileRoutes from "./routes/profiles-route.js";
 
 import Express from "express";
 const app = Express();
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use("/api/post", postRoutes);
 app.use("/api/like", likeRoutes);
 app.use("/api/comment", commentRoutes);
-app.use("/api/account", accountRoutes);
+app.use("/api/profile", profileRoutes);
 
 app.get("/", (req, res) => {
   res.send("Responding to request for main page.");
