c03520f6c4b4fe2f46c1a2c20cb4bbd87650d35f
JKosinski11 <Kosinski.jacob11@gmail.com>
2023-11-12 11:04:27 -0500

    Posts on exploere page

--- react-doodles/src/Pages/Explore.tsx
+++ react-doodles/src/Pages/Explore.tsx
@@ -1,9 +1,97 @@
+import { Container } from '@mui/material';
+import DisplayModeButtons from '../Components/DisplayModeButtons';
 import Heading from '../Components/Heading';
-import React from 'react';
+import React, { useState } from 'react';
+import Post from '../Components/Post';
+import styled from 'styled-components';
+
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
 function ExplorerPage(){
+    const [displayMode, setDisplayMode] = useState("friends");
+
+  const handleDisplayModeChange = (mode: string) => {
+    setDisplayMode(mode);
+    // You can add logic to fetch and display posts based on the selected mode
+  };
+    
+
     return(
+    <>
+            <div>
         <Heading text='Explore'></Heading>
-    )
+         
+         <AppContainer>
+        <DisplayModeButtons
+          displayMode={displayMode}
+          onDisplayModeChange={handleDisplayModeChange}
+        />
+        {/* Other content */}
+
+     
+        {/* Add Post components based on the displayMode */}
+        {displayMode === "friends" && (
+          <>
+            <Post
+              username="User123"
+              userProfilePic="Logo.png"
+              imageSrc="leaderboardPic1.png"
+              title="My Awesome Post"
+              description="Check out my latest creation!"
+            />
+            <Post
+              username="User456"
+              userProfilePic="Logo.png"
+              imageSrc="leaderboardPic2.png"
+              title="Another Post"
+              description="More amazing content!"
+            />
+            <Post
+              username="User789"
+              userProfilePic="Logo.png"
+              imageSrc="leaderboardPic3.png"
+              title="Great Post"
+              description="Don't miss it!"
+            />
+           
+          </>
+        )}
+        {/* Add more Post components based on the displayMode */}
+        {displayMode === "global" && (
+          <>
+            <Post
+              username="User456"
+              userProfilePic="Logo.png"
+              imageSrc="leaderboardPic2.png"
+              title="Another Post"
+              description="More amazing content!"
+            />
+            <Post
+              username="User789"
+              userProfilePic="Logo.png"
+              imageSrc="leaderboardPic3.png"
+              title="Great Post"
+              description="Don't miss it!"
+            />
+            <Post
+              username="User123"
+              userProfilePic="Logo.png"
+              imageSrc="leaderboardPic1.png"
+              title="My Awesome Post"
+              description="Check out my latest creation!"
+            />
+            {/* Add more global posts */}
+         </>
+        )}
+      
+      </AppContainer>
+      </div>  </>
+  );
 }
+
 export default ExplorerPage;
