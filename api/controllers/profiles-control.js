fbec0eae6742eddfbc85afa144b4b8ede5dec5be
Izaak Whetsell <izaakwhetsell@gmail.com>
2023-11-12 11:03:10 -0500

    started profile backend. Needs testing and completion.

--- api/controllers/profiles-control.js
+++ api/controllers/profiles-control.js
@@ -1,8 +1,116 @@
-import express from "express";
-import { getUser } from "../controller/user.js"
 
-const router = express.Router();
+import { dbConnection } from "../db-connect.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
 
-router.get("", );
 
-export default router;
+export const getProfile = (req, res) => {
+    const userId = req.query.userId;
+    const token = req.cookies.accessToken;
+    
+    if (!token) return res.status(401).json("Not logged in!");//Checks to see if user is logged in
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if(err) return res.status(403).json("Token is not valid!");//User have correct information?
+        
+        //SQL statement
+        const q = (userId !== "undefined" ? 'SELECT * FROM profiles AS p WHERE p.userID = ?;': '');//TODO: FILL IN WHERE THEY DONT HAVE USER ID
+
+        const values = userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
+
+        //Connect to database with statement and values
+        dbConnection.query(q, values, (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+            });
+    });
+};
+
+export const getName = (req, res) => {
+    const userId = req.query.userId;
+    const token = req.cookies.accessToken;
+    
+    if (!token) return res.status(401).json("Not logged in!");//Checks to see if user is logged in
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if(err) return res.status(403).json("Token is not valid!");//User have correct information?
+        
+        //SQL statement
+        const q = (userId !== "undefined" ? 'SELECT profile_name FROM profiles AS p WHERE p.userID = ?;': '');//TODO: FILL IN WHERE THEY DONT HAVE USER ID
+
+        const values = userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
+
+        //Connect to database with statement and values
+        dbConnection.query(q, values, (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+            });
+    });
+};
+
+export const getPic = (req, res) => {
+    const userId = req.query.userId;
+    const token = req.cookies.accessToken;
+    
+    if (!token) return res.status(401).json("Not logged in!");//Checks to see if user is logged in
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if(err) return res.status(403).json("Token is not valid!");//User have correct information?
+        
+        //SQL statement
+        const q = (userId !== "undefined" ? 'SELECT profile_image FROM profiles AS p WHERE p.userID = ?;': '');//TODO: FILL IN WHERE THEY DONT HAVE USER ID
+
+        const values = userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
+
+        //Connect to database with statement and values
+        dbConnection.query(q, values, (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+            });
+    });
+};
+
+export const setName = (req, res) => {
+    const userId = req.query.userId;
+    const token = req.cookies.accessToken;
+    
+    if (!token) return res.status(401).json("Not logged in!");//Checks to see if user is logged in
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if(err) return res.status(403).json("Token is not valid!");//User have correct information?
+        
+        //SQL statement
+        const q = (userId !== "undefined" ? 'UPDATE profiles AS p SET profile_name= ? WHERE p.userID = ?;': '');//TODO: FILL IN WHERE THEY DONT HAVE USER ID
+
+        const values = [,userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id]];//ADD new Profile name first
+
+        //Connect to database with statement and values
+        dbConnection.query(q, [values], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+            });
+    });
+};
+
+export const SetPic = (req, res) => {
+    const userId = req.query.userId;
+    const token = req.cookies.accessToken;
+    
+    if (!token) return res.status(401).json("Not logged in!");//Checks to see if user is logged in
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if(err) return res.status(403).json("Token is not valid!");//User have correct information?
+        
+        //SQL statement
+        const q = (userId !== "undefined" ? '': '');//TODO: FILL IN WHERE THEY DONT HAVE USER ID
+
+        const values = userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
+
+        //Connect to database with statement and values
+        dbConnection.query(q, values, (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+            });
+    });
+};
+
--- api/routes/profiles-route.js
+++ api/routes/profiles-route.js
@@ -1,9 +1,19 @@
 import express from "express";
+import { SetPic, getName, getPic, getProfile, setName } from "../controllers/profiles-control";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Responding to account related request.");
+  res.send("Responding to profile related request.");
 });
+
+router.get("/get-Profile", getProfile);//DOES THIS NEED /:userID in it??
+router.get("/get-name", getName);
+router.get("/get-prof-pic", getPic);
+
+router.update("/change-name", setName);
+router.update("/change-pic", SetPic);
+
+
 
 export default router;
