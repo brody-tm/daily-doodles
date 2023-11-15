ca53e31370436ca2fe7ea0b548da208b2707be6e
brodytrue <btm00005@mix.wvu.edu>
2023-11-15 18:56:35 -0500

    Modified delPost is post controls

--- api/controllers/post-control.js
+++ api/controllers/post-control.js
@@ -5,9 +5,7 @@ import moment from "moment";
 export const getPost = (req, res) => {
   const q = "SELECT * FROM DailyDoodlesDB.posts WHERE 'id' = ?";
 
-  const postId = req.body.postId;
-
-  dbConnection.query(q, postId, (queryError, results) => {
+  dbConnection.query(q, [req.body.postId], (queryError, results) => {
     if (queryError) {
       console.error("Error executing the query:", queryError);
       return;
@@ -76,19 +74,28 @@ export const addPost = (req, res) => {
 };
 
 export const delPost = (req, res) => {
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json("Not logged in!");
+  const q = "DELETE FROM posts WHERE `id`=? AND `user_id` = ?";
 
-  jwt.verify(token, "secretkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-
-    const q = "DELETE FROM posts WHERE `id`=? AND `userId` = ?";
-
-    dbConnection.query(q, [req.params.id, userInfo.id], (err, data) => {
-      if (err) return res.status(500).json(err);
-      if (data.affectedRows > 0)
-        return res.status(200).json("Post has been deleted.");
-      return res.status(403).json("You can delete only your post");
-    });
+  dbConnection.query(q, [req.body.id, req.body.user_id], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.affectedRows > 0)
+      return res.status(200).json("Post has been deleted.");
+    return res.status(403).json("You can delete only your post");
   });
+
+  // const token = req.cookies.accessToken;
+  // if (!token) return res.status(401).json("Not logged in!");
+
+  // jwt.verify(token, "secretkey", (err, userInfo) => {
+  //   if (err) return res.status(403).json("Token is not valid!");
+
+  //   const q = "DELETE FROM posts WHERE `id`=? AND `userId` = ?";
+
+  //   dbConnection.query(q, [req.params.id, userInfo.id], (err, data) => {
+  //     if (err) return res.status(500).json(err);
+  //     if (data.affectedRows > 0)
+  //       return res.status(200).json("Post has been deleted.");
+  //     return res.status(403).json("You can delete only your post");
+  //   });
+  // });
 };
