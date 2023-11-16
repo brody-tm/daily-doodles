590684f89b2a23725e5a0a537e23e419533ed330
ran2803 <ran00003@mix.wvu.edu>
2023-11-15 20:52:23 -0500

    changed explore import statement

--- react-doodles/src/Pages/Explore.tsx
+++ react-doodles/src/Pages/Explore.tsx
@@ -1,13 +1,12 @@
-import { Container } from '@mui/material';
-import DisplayModeButtons from '../Components/DisplayModeButtons';
-import Heading from '../Components/Heading';
-import React, { useState } from 'react';
-import Post from '../Components/Post';
-import styled from 'styled-components';
-import samplePic1 from "../leaderboardPic1.png";
-import samplePic2 from "../leaderboardPic2.png";
-import samplePic3 from "../leaderboardPic2.png";
-
+import { Container } from "@mui/material";
+import DisplayModeButtons from "../Components/DisplayModeButtons";
+import Heading from "../Components/Heading";
+import React, { useState } from "react";
+import Post from "../Components/Post";
+import styled from "styled-components";
+import samplePic1 from "../images/leaderboardPic1.png";
+import samplePic2 from "../images/leaderboardPic2.png";
+import samplePic3 from "../images/leaderboardPic2.png";
 
 const AppContainer = styled.div`
   display: flex;
@@ -15,85 +14,82 @@ const AppContainer = styled.div`
   justify-content: center;
 `;
 
-function ExplorerPage(){
-    const [displayMode, setDisplayMode] = useState("friends");
+function ExplorerPage() {
+  const [displayMode, setDisplayMode] = useState("friends");
 
   const handleDisplayModeChange = (mode: string) => {
     setDisplayMode(mode);
     // You can add logic to fetch and display posts based on the selected mode
   };
-    
 
-    return(
+  return (
     <>
-            <div>
-        <Heading text='Explore'></Heading>
-         
-         <AppContainer>
-        <DisplayModeButtons
-          displayMode={displayMode}
-          onDisplayModeChange={handleDisplayModeChange}
-        />
-        {/* Other content */}
+      <div>
+        <Heading text="Explore"></Heading>
 
-     
-        {/* Add Post components based on the displayMode */}
-        {displayMode === "friends" && (
-          <>
-            <Post
-              username="User123"
-              userProfilePic="../Logo.png"
-              imageSrc={samplePic1}
-              title="My Awesome Post"
-              description="Check out my latest creation!"
-            />
-            <Post
-              username="User456"
-              userProfilePic="../Logo.png"
-              imageSrc={samplePic2}
-              title="Another Post"
-              description="More amazing content!"
-            />
-            <Post
-              username="User789"
-              userProfilePic="../Logo.png"
-              imageSrc={samplePic3}
-              title="Great Post"
-              description="My amazing art"
-            />
-           
-          </>
-        )}
-        {/* Add more Post components based on the displayMode */}
-        {displayMode === "global" && (
-          <>
-            <Post
-              username="User456"
-              userProfilePic="../Logo.png"
-              imageSrc={samplePic3}
-              title="Another Post"
-              description="My masterpiece"
-            />
-            <Post
-              username="User789"
-              userProfilePic="../Logo.png"
-              imageSrc={samplePic2}
-              title="Great Post"
-              description="Harry potter"
-            />
-            <Post
-              username="User123"
-              userProfilePic="../Logo.png"
-              imageSrc={samplePic1}
-              title="My Awesome Post"
-              description="Just a silly little guy"
-            />
-            {/* Add more global posts */}
-         </>
-        )}
-      
-      </AppContainer>
-      </div>  </>
+        <AppContainer>
+          <DisplayModeButtons
+            displayMode={displayMode}
+            onDisplayModeChange={handleDisplayModeChange}
+          />
+          {/* Other content */}
+
+          {/* Add Post components based on the displayMode */}
+          {displayMode === "friends" && (
+            <>
+              <Post
+                username="User123"
+                userProfilePic="../Logo.png"
+                imageSrc={samplePic1}
+                title="My Awesome Post"
+                description="Check out my latest creation!"
+              />
+              <Post
+                username="User456"
+                userProfilePic="../Logo.png"
+                imageSrc={samplePic2}
+                title="Another Post"
+                description="More amazing content!"
+              />
+              <Post
+                username="User789"
+                userProfilePic="../Logo.png"
+                imageSrc={samplePic3}
+                title="Great Post"
+                description="My amazing art"
+              />
+            </>
+          )}
+          {/* Add more Post components based on the displayMode */}
+          {displayMode === "global" && (
+            <>
+              <Post
+                username="User456"
+                userProfilePic="../Logo.png"
+                imageSrc={samplePic3}
+                title="Another Post"
+                description="My masterpiece"
+              />
+              <Post
+                username="User789"
+                userProfilePic="../Logo.png"
+                imageSrc={samplePic2}
+                title="Great Post"
+                description="Harry potter"
+              />
+              <Post
+                username="User123"
+                userProfilePic="../Logo.png"
+                imageSrc={samplePic1}
+                title="My Awesome Post"
+                description="Just a silly little guy"
+              />
+              {/* Add more global posts */}
+            </>
+          )}
+        </AppContainer>
+      </div>{" "}
+    </>
   );
 }
 
