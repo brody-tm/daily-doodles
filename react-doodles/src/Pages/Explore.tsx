ad6152d9f26fc091b2ac8cdb397f00b6a6cc1722
ran2803 <ran00003@mix.wvu.edu>
2023-11-30 11:27:52 -0500

    tweeked import statement in Explore

--- react-doodles/src/Pages/Explore.tsx
+++ react-doodles/src/Pages/Explore.tsx
@@ -1,17 +1,16 @@
-
-import { Container } from '@mui/material';
-import DisplayModeButtons from '../Components/DisplayModeButtons';
-import Heading from '../Components/Heading';
-import React, { useState } from 'react';
-import Post from '../Components/Post';
-import styled from 'styled-components';
-import samplePic1 from "../leaderboardPic1.png";
-import samplePic2 from "../leaderboardPic2.png";
-import samplePic3 from "../leaderboardPic2.png";
+import { Container } from "@mui/material";
+import DisplayModeButtons from "../Components/DisplayModeButtons";
+import Heading from "../Components/Heading";
+import React, { useState } from "react";
+import Post from "../Components/Post";
+import styled from "styled-components";
+import samplePic1 from "../images/leaderboardPic1.png";
+import samplePic2 from "../images/leaderboardPic2.png";
+import samplePic3 from "../images/leaderboardPic2.png";
 import "../Styles/Explorer.css";
 
-//styles for conatiner 
-const AppContainer = styled.div`  
+//styles for conatiner
+const AppContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -20,10 +19,9 @@ const AppContainer = styled.div`
 function ExplorerPage() {
   //sets opening to friends page
   const [displayMode, setDisplayMode] = useState("friends");
-//changes basd on whethr global or friends button is selected
+  //changes basd on whethr global or friends button is selected
   const handleDisplayModeChange = (mode: string) => {
     setDisplayMode(mode);
-    
   };
 
   return (
