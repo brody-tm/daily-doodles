4820b97bd4e212aa398928fe7c105cf700d63698
Izaak Whetsell <izaakwhetsell@gmail.com>
2023-12-05 00:51:38 -0500

    Able to display hardcoded names/bio on profile page.

--- react-doodles/src/Components/ProfileLeft.tsx
+++ react-doodles/src/Components/ProfileLeft.tsx
@@ -1,26 +1,88 @@
 import Pallet from "../Components/profilePallet";
+import EditProfilePopup from "./EditProfile";
+
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+type Profile = {//alter to fit with database
+  profile_name: string;
+  profile_image: string;
+  bio: string;
+  likes: number;
+  follows: number;
+  
+};
 
 function LeftSide() {
+  const navigate = useNavigate(); //TEMP
+  const [loginPopupVisible, setEditPopupVisible] = useState(false);
+  const [profile, setProfile] = useState<Profile[]>([]);
+  
+  useEffect(() => { 
+    const GetProfileData = async () => {
+      // attempt to send the request
+      try {
+        const res = await fetch("http://localhost:8800/api/profile/get-profile", {
+          method: "GET"
+        });
+  
+        console.log(res);
+        // check response
+        if (!res.ok) {
+          throw new Error(`HTTP error on login: ${res.status}`);
+        }
+        const resData = await res.json();
+        setProfile(resData);
+      } catch (err) {
+        console.error("Error sending login request:", err);
+      }
+    };
+    GetProfileData();
+  }, []);
+  
+  //Login handlers for opening and closing the window
+  const openEditPopup = () => {
+    setEditPopupVisible(true);
+  };
+
+  const closeEditPopup = () => {
+    setEditPopupVisible(false);
+  };
+
+
   var name = "John Doe";
   var description = "Just here to have a good time";
   return (
-    <div className="column" id="column1">
-      <ul className="leftgroup">
-        <li className="list-group-item">{name}</li>
-        <li className="list-group-item">{description}</li>
-        <li className="list-group-item">
-          <Pallet></Pallet>
-        </li>
-        <li className="list-group-item">
-          <button>Edit Profile</button>
-        </li>
-        <li className="list-group-item">
-          <button onClick={handleLogout}>Sign Out</button>
-        </li>
-      </ul>
-    </div>
+    <>
+    {profile.map((profile) => (
+      <>
+      <div className="column" id="column1">
+        <ul className="leftgroup">
+          <li className="list-group-item"><strong>{profile.profile_name}</strong></li>
+          <li className="list-group-item">{profile.bio}</li>
+          <li className="list-group-item">
+            <Pallet></Pallet>
+          </li>
+          <li className="list-group-item">
+            <button onClick={openEditPopup}>Edit Profile</button>
+            </li>
+            <li className="list-group-item">
+              <button onClick={handleLogout}>Sign Out</button>
+            </li>
+          </ul>
+        </div>
+      </>
+      ))}
+    
+    {loginPopupVisible && (
+       <EditProfilePopup onClose={closeEditPopup}  />
+      )}
+    </>
+    
   );
 }
+
+
 
 const handleLogout = async () => {
   try {
